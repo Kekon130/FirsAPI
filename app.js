f9cbed74dfaf0df958d5bae7031249833c4f69b1
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2022-03-03 09:53:37 +0100

    Body-parser añadido y añadida la primera ruta

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 //Asignamos la dirección de las rutas del usuario a una variable
 const userRoutes = require('./Routes/userRoutes');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use ('/user', userRoutes);
+
+module.exports = app;
