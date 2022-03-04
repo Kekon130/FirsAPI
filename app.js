4e7bd893cc7b0c9cbf284fa9f246e439a8f3f9d0
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2022-03-04 18:50:45 +0100

    Eslint añadido al proyecto y completado el fichero server.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-//Asignamos la dirección de las rutas del usuario a una variable
+// Asignamos la dirección de las rutas del usuario a una variable
 const userRoutes = require('./Routes/userRoutes');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use ('/user', userRoutes);
+
+app.use('/user', userRoutes);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const mongoose = require('mongoose');
-const app = require('./app');
+/* eslint-disable no-console */
+const mongoose = require('mongoose'); // Importamos mongoose
+const app = require('./app'); // Importamos el fichero app.js
 
-const mongodb;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // Asignamos el puerto en el que levantaremos la API
+const mongodb = process.mongoose.PORT || 'mongodb://localhost:27017/ToDo-API'; // Asignamos el puerto en el que se alojará la base de datos
+
+// Conectamos la API con la base de datos en Mongo
+mongoose.connect(mongodb, (err) => {
+  if (err) {
+    console.log('Unable to connect to database.'); // Si ocurre un error durante la conexión se mostrará un error en la consola
+  } else {
+    app.connect(port, () => {
+      console.log('API running.'); // Si todo va bien se mostrará un mensaje por pantalla
+    });
+  }
+});
