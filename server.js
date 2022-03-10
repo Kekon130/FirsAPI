98db5c7dca2c813a1734fa91e609e192c35b37a3
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2022-03-10 16:29:19 +0100

    Por fin conecta con Mongo

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = require('./app');
 // Asignamos el puerto en el que levantaremos la API
 const port = process.env.PORT || 3000;
 // Asignamos el puerto en el que se alojará la base de datos
-const mongodb = process.env.MONGODB || 'mongodb://localhost:27017/ToDo-API';
+const mongodb = process.env.MONGODB || 'mongodb://127.0.0.1:27017/FirstAPI';
 
 // Conectamos la API con la base de datos en Mongo
-mongoose.connect(mongodb, (err) => {
+mongoose.connect(mongodb, { useNewUrlParser: true }, (err) => {
   // Si ocurre un error durante la conexión se mostrará un error en la consola
   if (err) console.log('Unable to connect to database.');
   // Si todo va bien se mostrará un mensaje por pantalla
-  else app.connect(port, () => { console.log('API running.'); });
+  else app.listen(port, () => { console.log(`Node server running on http://localhost:${port}`); });
 });
