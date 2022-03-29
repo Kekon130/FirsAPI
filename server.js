ff8633b198cb7ae3237f99513b7629f85c5b3029
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2022-03-29 16:23:44 +0200

    dotenv listo y cambios en el .gitignore

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-console */
+// Usamos dotenv para leer variables de entorno
+require('dotenv').config();
 // Importamos mongoose
 const mongoose = require('mongoose');
 // Importamos el fichero app.js
@@ -6,7 +8,7 @@ const app = require('./app');
 // Asignamos el puerto en el que levantaremos la API
 const port = process.env.PORT || 3000;
 // Asignamos el puerto en el que se alojará la base de datos
-const mongodb = process.env.MONGODB || 'mongodb://127.0.0.1:27017/FirstAPI';
+const mongodb = process.env.MONGODB || '0.0.0.0';
 
 // Conectamos la API con la base de datos en Mongo
 mongoose.connect(mongodb, { useNewUrlParser: true }, (err) => {
