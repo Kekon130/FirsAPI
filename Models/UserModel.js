6379484fefd0908187a875f7418764b102a1a9c1
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2022-03-29 11:50:15 +0200

    Implementado el cifrado de contraseñas

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 // Importamos la clase Schema del paquete mongoose
 const mongoose = require('mongoose');
 // Declaramos el esquema del usuario
@@ -12,11 +13,11 @@ const userSchema = new Schema({
   email: {
     type: String, lowercase: true, unique: true, required: [true, 'email required'],
   },
-  password: { type: String, required: [true, 'password required'] },
-  salt: { typ: String },
+  password: { type: String, select: false, required: [true, 'password required'] },
+  salt: { type: String, select: false },
 });
 
-userSchema.pre('save', (next) => {
+userSchema.pre('save', function (next) {
   if (this.isModified('password')) {
     this.salt = randomBytes(64).toString('hex');
     this.password = pbkdf2Sync(this.password, this.salt, 1000, 64, 'sha512').toString('hex');
