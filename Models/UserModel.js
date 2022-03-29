e3905a0074a76bb25b5f218d5a2337ea9b5e8dfa
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2022-03-29 11:11:51 +0200

    Intento de cifrar contraseñas

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -2,12 +2,26 @@
 const mongoose = require('mongoose');
 // Declaramos el esquema del usuario
 const { Schema } = mongoose;
+const { pbkdf2Sync, randomBytes } = require('crypto');
 
 const userSchema = new Schema({
   name: { type: String },
   surname: { type: String },
-  email: { type: String },
   age: { type: Number },
+  username: { type: String, unique: true, required: [true, 'username required'] },
+  email: {
+    type: String, lowercase: true, unique: true, required: [true, 'email required'],
+  },
+  password: { type: String, required: [true, 'password required'] },
+  salt: { typ: String },
+});
+
+userSchema.pre('save', (next) => {
+  if (this.isModified('password')) {
+    this.salt = randomBytes(64).toString('hex');
+    this.password = pbkdf2Sync(this.password, this.salt, 1000, 64, 'sha512').toString('hex');
+  }
+  next();
 });
 // Exportamos el modelo del usuario
 module.exports = mongoose.model('User', userSchema);
